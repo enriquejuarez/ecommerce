beaed4c8aa1444c60fb4577e63c5a76e7535c71d
caenjuji <caenjuji@gmail.com>
2019-05-09 14:03:30 -0500

    Se agrega producto al carrito

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,9 @@
 //Variables
 let elem;
-let productos;
 let carrito =  document.querySelector('#carrito');
 let vista_carrito =  document.querySelector('#vista-carrito');
+let lista_productos = document.getElementById('lista-productos');
+let lista_carrito =  document.querySelector('#vista-carrito table tbody');
 
 //Event Listener
 document.addEventListener('DOMContentLoaded', function() {
@@ -19,7 +20,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
                                                 let elemento = data.childNodes[3].childNodes[1];
                                                 if (!elemento.classList.contains('fadeInLeft')){
-                                                    console.log("Entrad")
                                                     elemento.classList.remove('hide');
                                                     elemento.classList.add('fadeInLeft');
                                                 }
@@ -27,12 +27,9 @@ document.addEventListener('DOMContentLoaded', function() {
                                     });
     autoplay(); //Inicia carousel
 
-    productos = document.querySelectorAll('.add-carrito'); //Agregar al carrito de compra
-    productos.forEach(function(producto) {
-        producto.addEventListener('click', agregarCarrito);
-    });
+    carrito.addEventListener('click', mostrarOcultarCarrito);
 
-    carrito.addEventListener('click', mostrarCarrito);
+    lista_productos.addEventListener('click', agregarCarrito);
 });
 
 
@@ -43,14 +40,51 @@ function autoplay() {
     setTimeout(autoplay, 6500);
 }
 
-function agregarCarrito(){
-    M.toast({
-        html: 'Agregado al carrito!',
-        inDuration: 400
-    });
-}
-
-function mostrarCarrito(e){
-    console.log(vista_carrito);
+function mostrarOcultarCarrito(e){
     vista_carrito.classList.toggle('hide');
 }
+
+function agregarCarrito(e){
+    e.preventDefault();
+
+    if (e.target.parentElement.classList.contains('add-carrito')) {
+        const producto = e.target.parentElement.parentElement.parentElement;
+        leerDatosProducto(producto);
+
+        M.toast({
+            html: 'Agregado al carrito!',
+            inDuration: 400
+        });
+    }
+}
+
+
+function leerDatosProducto(producto){
+    
+    const infoProducto = {
+        imagen: producto.querySelector('.card-image img').src,
+        titulo: producto.querySelector('.card-content .card-title a').textContent,
+        precio: producto.querySelector('.card-content .precios .precio-actual').textContent,
+        id: producto.getAttribute('data-id')
+    }
+    insertarcarrito(infoProducto);
+    console.log(infoProducto);
+}
+
+function insertarcarrito(infoProducto){
+    const row = document.createElement('tr');
+
+    row.innerHTML = `
+        <td>
+            <img src="${infoProducto.imagen}" class="responsive-img">
+        </td>
+        <td>${infoProducto.titulo}</td>
+        <td><em>${infoProducto.precio}</em></td>
+        <td>
+            <a href="#" class="borrar-curso" data-id="${infoProducto.id}"><strong>X</strong></a>
+        </td>
+    `;
+    lista_carrito.appendChild(row);
+    // guardarCursoLocalStorage(curso);
+}
+                                          
